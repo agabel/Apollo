1f00be48a23a95d83278e93e956e312f562520ec
agabel <agabel@gmail.com>
2016-03-21 21:44:46 -0500

    Listen on all interfaces. Added debug print statements.

--- player.py
+++ player.py
@@ -7,7 +7,7 @@ import select
 
 from loggers import server_logger
 
-HOST = '127.0.0.1'
+HOST = ''
 PORT = 50023
 
 _CONNECTIONS = {}
@@ -31,19 +31,22 @@ AUDIO_STREAM = AudioStream()
 def play_audio_frames():
     while True:
         if len(FRAMES):
+            print("playing audio frame")
             frame = FRAMES.pop(0)
             AUDIO_STREAM.stream.write(frame)
 
 def process_audio():
+    print("processing audio")
     audio_thread = thread.start_new_thread(play_audio_frames)
 
 
 def create_server_socket():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    #s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind((HOST, PORT))
     s.listen(1)
-    s.setblocking(0)
+    #s.setblocking(0)
+    print("Server listening...")
 
     return s
 
@@ -58,6 +61,7 @@ def start_server():
     while True:
         data = conn.recv(88200)
         if data != '':
+            print("Received data")
             try:
                 FRAMES.append(data)
             except Exception as e:
